36a9ac7816904731a08abd57a45a659f60fe7ace
Ola <owaniczek.it@gmail.com>
2023-10-07 22:11:18 +0200

    firebase beginning

---
+++ src/App/Firebase/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCpVy4hSUZGYHNtTMKmOqDvZSxO9yH5YwI",
+  authDomain: "pomeranian-ola.firebaseapp.com",
+  projectId: "pomeranian-ola",
+  storageBucket: "pomeranian-ola.appspot.com",
+  messagingSenderId: "622828019405",
+  appId: "1:622828019405:web:09267a125766f1a41c1384"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
