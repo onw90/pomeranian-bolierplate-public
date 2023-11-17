7d284952e8164d15b73b065fe00e7c3cd74b17f8
Ola <owaniczek.it@gmail.com>
2023-11-17 22:58:56 +0100

    cv edited

---
+++ src/App/CV/Description.jsx
@@ -0,0 +1,326 @@
+import './description.css';
+
+export const Pomeranian = () => {
+  return (
+    <div className="certificate">
+      <p>
+        An{' '}
+        <span className="keyword">
+          intensive three-month programming course
+        </span>
+        , led by experienced trainers, encompassed numerous hours of live coding
+        and hands-on work on diverse practical projects. This course provided
+        participants with a{' '}
+        <span className="keyword">solid and comprehensive foundation</span>{' '}
+        necessary for launching a career in the IT industry as a Frontend
+        Developer. The main technologies covered were{' '}
+        <span className="keyword">
+          HTML5, CSS3, JavaScript, React, and TypeScript
+        </span>
+        . Students also utilized tools such as{' '}
+        <span className="keyword">
+          Git, GitHub, Redmine, JIRA, and Confluence
+        </span>
+        .
+      </p>
+    </div>
+  );
+};
+
+export const Aptiv = () => {
+  return (
+    <section className="key-expertise">
+      <ul>
+        <li>
+          <span className="keyword">
+            Design, Develop, and Evaluate Optical Test Systems
+          </span>{' '}
+          based on LED and laser technologies
+        </li>
+        <li>
+          <span className="keyword">Specify and Source</span> lenses, sensors,
+          LEDs, VCSELs, and lasers working in the visible and IR spectrum
+        </li>
+        <li>
+          <span className="keyword">Conduct Optical and Light Simulations</span>{' '}
+          using Light Tools
+        </li>
+        <li>
+          <span className="keyword">Evaluate Vision System Performance</span>,
+          including MTF, S/N, aberrations, and other quality parameters
+        </li>
+        <li>
+          <span className="keyword">Review Customer Specifications</span> and
+          document requirements
+        </li>
+        <li>
+          <span className="keyword">Develop Component Specifications</span>{' '}
+          based on requirements
+        </li>
+        <li>
+          <span className="keyword">Calculate Eye Safety Coefficients</span> to
+          approve the light source for use
+        </li>
+        <li>
+          <span className="keyword">Collaborate Within a Team</span> of optical
+          and mechanical engineers
+        </li>
+      </ul>
+    </section>
+  );
+};
+
+export const Vidium = () => {
+  return (
+    <section className="key-expertise">
+      <ul>
+        <li>
+          <span className="keyword">
+            Calculated the power of implantable lenses
+          </span>
+          , including IOL (all types) and ICL (Staar)
+        </li>
+        <li>
+          <span className="keyword">Planned refractive surgery procedures</span>{' '}
+          using femtosecond and excimer lasers, such as FS200 and EX500
+          (Wavelight Alcon), MEL90 and Visumax (Zeiss)
+        </li>
+        <li>
+          <span className="keyword">
+            Operated lasers and performed laser testing
+          </span>{' '}
+          as a technician, including calculating nomograms for operators and
+          lasers
+        </li>
+        <li>
+          <span className="keyword">Selected specialized contact lenses</span>,
+          including scleral and hybrid types
+        </li>
+        <li>
+          <span className="keyword">
+            Operated and worked with measurement devices
+          </span>{' '}
+          such as Pentacam, Oculyzer, OCT, IOL Master, and other biometers,
+          Verion system, specular microscope, autorefractometer, tonometers,
+          including ORA (Reichert) with corneal hysteresis analysis, slit lamps,
+          and other ophthalmic devices
+        </li>
+        <li>
+          <span className="keyword">
+            Understood and planned patient diagnosis and therapy
+          </span>{' '}
+          in cooperation with the doctor
+        </li>
+        <li>
+          <span className="keyword">Conducted tests and measurements</span> to
+          qualify patients for refractive surgery, cataract procedures, AMD, and
+          keratoconus
+        </li>
+        <li>
+          <span className="keyword">
+            Performed subjective refraction measurements
+          </span>{' '}
+          using various phoropters
+        </li>
+        <li>
+          <span className="keyword">
+            Conducted specialized training sessions
+          </span>{' '}
+          for both medical and non-medical personnel
+        </li>
+        <li>
+          <span className="keyword">Planned the work of a team</span> of
+          optometrists, both in terms of their responsibilities and organization
+        </li>
+        <li>
+          <span className="keyword">
+            Demonstrated the ability to select appropriate equipment
+          </span>{' '}
+          for an ophthalmic-optometric center, oversaw its installation and
+          operation, and engaged in business planning
+        </li>
+        <li>
+          <span className="keyword">
+            Developed optometric and medical procedures
+          </span>{' '}
+          within an ophthalmic center, specializing in areas such as refractive
+          surgery or other specializations
+        </li>
+        <li>
+          <span className="keyword">
+            Evaluated the progress and performance
+          </span>{' '}
+          of the team and reported to the COO and CEO
+        </li>
+        <li>
+          <span className="keyword">
+            Identified and defined new opportunities
+          </span>{' '}
+          for improving business processes based on data analysis
+        </li>
+        <li>
+          <span className="keyword">Conducted recruitment processes</span> for
+          positions such as optometrists, nurses, and receptionists in a medical
+          clinic
+        </li>
+        <li>
+          <span className="keyword">Used analytical skills</span> to analyze
+          patient results, maintained statistics, drew conclusions, and created
+          reports
+        </li>
+        <li>
+          <span className="keyword">
+            Presented personal knowledge and experience
+          </span>{' '}
+          at scientific conferences and wrote scientific papers
+        </li>
+        <li>
+          <span className="keyword">Managed people, time, and work</span> to
+          meet deadlines and ensure effective planning
+        </li>
+        <li>
+          <span className="keyword">Developed personal expertise</span> and
+          shared it with team members to support team growth
+        </li>
+      </ul>
+    </section>
+  );
+};
+
+export const Bellevue = () => {
+  return (
+    <section className="key-expertise">
+      <ul>
+        <li>
+          <span className="keyword">Assembled various types of spectacles</span>{' '}
+          using the Essilor Kappa Edging System, manual grinder, and automatic
+          dioptometer
+        </li>
+        <li>
+          <span className="keyword">Selected frames and eyeglass lenses</span>{' '}
+          for each prescription
+        </li>
+        <li>
+          <span className="keyword">Sold eyeglasses and contact lenses</span> as
+          medical products
+        </li>
+        <li>
+          <span className="keyword">Provided optical consultations</span>
+        </li>
+        <li>
+          <span className="keyword">Handled office work</span> related to
+          incoming goods and inventory
+        </li>
+      </ul>
+    </section>
+  );
+};
+
+export const Alicante = () => {
+  return (
+    <section classNameName="key-expertise">
+      <ul>
+        <li>
+          <span class="keyword">
+            Conducted practices associated with optometric measurements
+          </span>
+          : refraction, keratometry, axial length measurements of the eye,
+          optical system aberrations of the eye, pachymetry, tonometry, and many
+          others, using:
+          <ul className="competition-list">
+            <li>Pentacam Scheimpflug camera (Oculus)</li>
+            <li>OCT</li>
+            <li>Wavefront analyzer VX120 (Visionix)</li>
+            <li>Slit lamp</li>
+            <li>Keratometers (e.g., IOL Master)</li>
+          </ul>
+        </li>
+        <li>
+          <span class="keyword">
+            Conducted qualitative and statistical data analysis
+          </span>
+        </li>
+        <li>
+          <span class="keyword">
+            Completed a publication and practice report
+          </span>{' '}
+          evaluated with an excellent grade
+        </li>
+      </ul>
+    </section>
+  );
+};
+
+export const Kassel = () => {
+  return (
+    <section classNameName="key-expertise">
+      <ul>
+        <li>
+          <span class="keyword">Engaged in student practice</span> related to
+          optical spectroscopy of semiconductor nanostructures
+        </li>
+        <li>
+          <span class="keyword">
+            Conducted optical (PL, μPL) and structural (AFM, XRD)
+            characterization
+          </span>
+        </li>
+        <li>
+          <span class="keyword">
+            Completed the practice with an excellent grade
+          </span>{' '}
+          for the practice report
+        </li>
+      </ul>
+    </section>
+  );
+};
+
+export const ClinicaOptica = () => {
+  return (
+    <section classNameName="key-expertise">
+      <ul>
+        <li>
+          <span class="keyword">
+            Cooperated with customers and engineering craftsmen
+          </span>
+        </li>
+        <li>
+          <span class="keyword">Assembled glasses</span>, which involved
+          handling the grinding machine Briot, manual grinder, and the telescope
+          dioptometer for various types of spectacles
+        </li>
+        <li>
+          <span class="keyword">
+            Selected appropriate spectacle lenses and lens coatings
+          </span>
+        </li>
+      </ul>
+    </section>
+  );
+};
+
+export const Master = () => {
+  return (
+    <section classNameName="education">
+      <p>
+        <span class="keyword">Optics and Optometry specialization</span>, Master
+        of Science degree.
+      </p>
+      <p>Graduated with the highest grade and honors from the Rector.</p>
+      <p>Master's Thesis written in English.</p>
+    </section>
+  );
+};
+
+export const Engineer = () => {
+  return (
+    <section classNameName="education">
+      <p>
+        <span class="keyword">Optics and Optometry specialization</span>,
+        Engineer’s degree.
+      </p>
+      <p>Graduated with the highest grade and honors from the Rector.</p>
+    </section>
+  );
+};
